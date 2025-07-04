9652f1850bcb2955c463ccf18deb21ff95b181ab
imdanpinkman <bullfroginkorea@gmail.com>
2025-07-04 14:24:32 +0900

    채팅방 스크롤 아래로 설정

--- lib/presentation/core/widget/chat/chat_message_list_view.dart
+++ lib/presentation/core/widget/chat/chat_message_list_view.dart
@@ -9,7 +9,7 @@ import 'chat_message_bubble.dart';
 import 'package:what_is_your_eta/data/model/message_model.dart';
 import 'package:what_is_your_eta/data/model/user_model.dart';
 
-class ChatMessageListView extends StatelessWidget {
+class ChatMessageListView extends StatefulWidget {
   final RxList<MessageModel> messages;
   final Map<String, UserModel> userMap;
   final String myUid;
@@ -22,16 +22,53 @@ class ChatMessageListView extends StatelessWidget {
   });
 
   @override
+  State<ChatMessageListView> createState() => _ChatMessageListViewState();
+}
+
+class _ChatMessageListViewState extends State<ChatMessageListView> {
+  final ScrollController _scrollController = ScrollController();
+
+  void _scrollToBottom() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (_scrollController.hasClients) {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeOut,
+        );
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollToBottom();
+    });
+    widget.messages.listen((_) => _scrollToBottom());
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => ListView.builder(
+    return Obx(() {
+      final messages = widget.messages;
+      return ListView.builder(
+        controller: _scrollController,
         itemCount: messages.length,
         itemBuilder: (context, index) {
           final msg = messages[index];
-          final isMe = msg.senderId == myUid;
-
+          final isMe = msg.senderId == widget.myUid;
           final sender =
-              msg.type == MessageType.system ? null : userMap[msg.senderId];
+              msg.type == MessageType.system
+                  ? null
+                  : widget.userMap[msg.senderId];
 
           if (sender == null && msg.type != MessageType.system) {
             return const SizedBox();
@@ -60,7 +97,7 @@ class ChatMessageListView extends StatelessWidget {
             },
           );
         },
-      ),
-    );
+      );
+    });
   }
 }
